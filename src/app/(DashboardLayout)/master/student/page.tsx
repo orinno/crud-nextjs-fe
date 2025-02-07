54b95eb276801c809c8e763aa88790d546fc4233
Kahla Luthfiyah Halim <kahla.luthfiyah22@smk.belajar.id>
2025-02-07 14:15:34 +0700

    kahla-perbaikan

--- src/app/(DashboardLayout)/master/student/page.tsx
+++ src/app/(DashboardLayout)/master/student/page.tsx
@@ -1,50 +1,47 @@
-<<<<<<< HEAD
-import React from "react";
-import List from "./List";
-
-export default function Page() {
-    return (
-        <div>
-            <List />
-        </div>
-    );
-}
-=======
 "use client";
 import React, { useEffect, useState } from "react";
 import List from "./List";
 
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
-
 export default function Page() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    // Fetch data dari API
-    const fetchData = async () => {
-      const res = await fetch("/api/users");
-      const data: User[] = await res.json();
-      setUsers(data);
-    };
-
-    fetchData();
-  }, []);
   return (
     <div>
-      <h1>Daftar Pengguna</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
-      {/* <List /> */}
+      <List />
     </div>
   );
 }
->>>>>>> 50078bb6bc35d290e3fcfebc619bfced7f0c452a
+
+
+// type User = {
+//   id: number;
+//   name: string;
+//   email: string;
+// };
+
+// export default function Page() {
+//   const [users, setUsers] = useState<User[]>([]);
+
+//   useEffect(() => {
+//     // Fetch data dari API
+//     const fetchData = async () => {
+//       const res = await fetch("/api/users");
+//       const data: User[] = await res.json();
+//       setUsers(data);
+//     };
+
+//     fetchData();
+//   }, []);
+//   return (
+//     <div>
+//       <h1>Daftar Pengguna</h1>
+//       <ul>
+//         {users.map((user) => (
+//           <li key={user.id}>
+//             {user.name} - {user.email}
+//           </li>
+//         ))}
+//       </ul>
+//       {/* <List /> */}
+//     </div>
+//   );
+// }
+
